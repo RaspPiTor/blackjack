5269a59d9a285218c9dd4f33e30c2c4168b73c8a
Alex Williams <hughaw64@gmail.com>
2018-11-29 20:44:29 +0000

    Updated code

--- game_manager.py
+++ game_manager.py
@@ -5,35 +5,52 @@ class Manager():
     def __init__(self):
         self.deck = []
         for suit in ['Heart', 'Club', 'Diamond', 'Spade']:
-            for number in 'AJQK'+''.join(map(str, range(1, 10))):
+            for number in 'AJQK'+''.join(map(str, range(2, 10))):
                 self.deck.append((suit, number))
         self.position = 0
         self.dealer = []
         self.player = []
-    def draw_one_player(self):
-        self.player.append(self.deck[self.position])
+
+    def draw_one(self, player=True):
+        hand = [self.dealer, self.player][int(bool(player))]
+        hand.append(self.deck[self.position])
         self.position += 1
-    def draw_one_dealer(self):
-        self.dealer.append(self.deck[self.position])
-        self.position += 1
+
     def start(self):
         random.shuffle(self.deck)
         self.position = 0
         self.dealer = []
         self.player = []
-        for _ in range(2):
-            self.draw_one_player()
-            self.draw_one_dealer()
-    def player_totals(self):
-        base = sum(int(i) for _, i in self.player if i.isdigit())
-        base += sum(10 for _,i in self.player if i in 'JQK')
-        a_count = sum(1 for _,i in self.player if i == 'A')
+        for hand in [False, True] * 2:
+            self.draw_one(hand)
+
+    def totals(self, player=True):
+        hand = [self.dealer, self.player][int(bool(player))]
+        base = sum(int(i) for _, i in hand if i.isdigit())
+        base += sum(10 for _,i in hand if i in 'JQK')
+        a_count = sum(1 for _,i in hand if i == 'A')
         if a_count:
             options = []
             for option in itertools.product([1,11], repeat=a_count):
                 options.append(base + sum(option))
         else:
             options = [base]
-        return set(options)
+        return list(filter(lambda x: x<=21, set(options)))
+
     def winner(self):
-        pass
+        player_total = self.totals(True)
+        dealer_twist = True
+        while dealer_twist:
+            dealer_total = self.totals(False)
+            if dealer_total and min(dealer_total) < 17:
+                self.draw_one(False)
+            else:
+                dealer_twist = False
+        if not player_total:
+            return 'DEALER'
+        if not dealer_total:
+            return 'PLAYER'
+        if max(player_total) > max(dealer_total):
+            return 'PLAYER'
+        else:
+            return 'DEALER'
--- main.py
+++ main.py
@@ -1,12 +1,61 @@
-##from tkinter import messagebox
-##from tkinter import ttk
-##import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import tkinter as tk
 
 import game_manager
 
-manager = game_manager.Manager()
-manager.start()
-for i in range(2):
-    manager.draw_one_player()
-print(manager.player_totals(), manager.player)
-print('Working')
+class GUI(ttk.Frame):
+    def __init__(self):
+        ttk.Frame.__init__(self)
+        score_frame = ttk.Frame(self)
+        score_frame.grid(row=0, column=0, columnspan=10)
+        ttk.Label(score_frame, text='Dealer score:').grid(row=0, column=0)
+        self.dealer_score = ttk.Label(score_frame, text='0')
+        self.dealer_score.grid(row=0, column=1)
+        ttk.Label(score_frame, text='Player score:').grid(row=0, column=2)
+        self.player_score = ttk.Label(score_frame, text='0')
+        self.player_score.grid(row=0, column=3)
+
+        ttk.Label(self, text='Your Hand').grid(row=1, column=0, sticky='w')
+        ttk.Label(self, text='Totals:').grid(row=1, column=1)
+        self.totals = ttk.Label(self)
+        self.totals.grid(row=1, column=2)
+        self.cards = ttk.Label(self)
+        self.cards.grid(row=2, column=0, columnspan=2, sticky='w')
+        ttk.Button(self, text='Stick', command=self.stick).grid(row=3, column=0)
+        ttk.Button(self, text='Draw', command=self.draw).grid(row=3, column=1)
+        
+        self.manager = game_manager.Manager()
+        self.start()
+    def render_hand(self):
+        self.cards['text'] = '\n'.join(map(lambda x: '%s of %s' % (x[1], x[0]),
+                                           self.manager.player))
+        self.totals['text'] = ', '.join(map(str, self.manager.totals(True)))
+    def start(self):
+        self.manager.start()
+        self.render_hand()
+    def stick(self):
+        winner = self.manager.winner()
+        if winner == 'PLAYER':
+            self.player_score['text'] = str(int(self.player_score['text']) + 1)
+            messagebox.Message(self, message='You won').show()
+        else:
+            self.dealer_score['text'] = str(int(self.dealer_score['text']) + 1)
+            messagebox.Message(self, message='Dealer won').show()
+        self.start()
+    def draw(self):
+        if self.manager.totals(True):
+            self.manager.draw_one()
+            self.render_hand()
+            if self.manager.totals(True):
+                self.render_hand()
+            else:
+                self.stick()
+        else:
+            self.stick()
+
+
+if __name__ == '__main__':
+    gui = GUI()
+    gui.grid()
+    gui.mainloop()
